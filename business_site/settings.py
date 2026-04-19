eb767367c75ff291e36075af07ebc60fba58a6c6
Yusuf Shaw <your-email@gmail.com>
2026-04-19 03:58:09 -0400

    Temporarily enable debug

--- business_site/settings.py
+++ business_site/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-fallback-key')
 
-DEBUG = False
+DEBUG = True
 
 # IMPORTANT: Add your domains here
 ALLOWED_HOSTS = [
