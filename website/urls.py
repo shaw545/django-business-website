3c8de5139f4b86cde4c8a28b19297479fa5024ab
Yusuf Shaw <your-email@gmail.com>
2026-04-19 06:43:54 -0400

    Fix website urls

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,10 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('website.urls')),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('services/', views.services, name='services'),
+    path('contact/', views.contact, name='contact'),
+    path('portfolio/', views.portfolio, name='portfolio'),
 ]
