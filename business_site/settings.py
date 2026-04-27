932008a3d359ed5e2407c185beb5fdc58eef15d5
Yusuf Shaw <your-email@gmail.com>
2026-04-27 17:49:15 -0400

    Temporarily enable debug

--- business_site/settings.py
+++ business_site/settings.py
@@ -5,8 +5,7 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-change-this-key")
-DEBUG = os.environ.get("DEBUG", "False") == "True"
-
+DEBUG = True
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "localhost",
