fc6a8ab0c712a73326337392a889843e1b3df84a
Yusuf Shaw <your-email@gmail.com>
2026-04-27 22:27:02 -0400

    Fix multiple product image upload form

--- website/forms.py
+++ website/forms.py
@@ -1,8 +1,62 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User, Group
+
 from .models import Product
 
 
-class MultipleFileInput(forms.FileInput):
+class SellerRegistrationForm(UserCreationForm):
+    SELLER_TYPE_CHOICES = [
+        ("personal", "Personal Seller"),
+        ("business", "Business Seller"),
+    ]
+
+    seller_type = forms.ChoiceField(
+        choices=SELLER_TYPE_CHOICES,
+        widget=forms.RadioSelect,
+        required=True,
+        label="Seller Type"
+    )
+
+    first_name = forms.CharField(max_length=100, required=True)
+    last_name = forms.CharField(max_length=100, required=True)
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = [
+            "username",
+            "first_name",
+            "last_name",
+            "email",
+            "seller_type",
+            "password1",
+            "password2",
+        ]
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+
+        user.first_name = self.cleaned_data["first_name"]
+        user.last_name = self.cleaned_data["last_name"]
+        user.email = self.cleaned_data["email"]
+
+        if commit:
+            user.save()
+
+            seller_type = self.cleaned_data.get("seller_type")
+
+            if seller_type == "business":
+                group, created = Group.objects.get_or_create(name="Business Sellers")
+            else:
+                group, created = Group.objects.get_or_create(name="Personal Sellers")
+
+            user.groups.add(group)
+
+        return user
+
+
+class MultipleFileInput(forms.ClearableFileInput):
     allow_multiple_selected = True
 
 
@@ -11,9 +65,21 @@ class MultipleFileField(forms.FileField):
         kwargs.setdefault("widget", MultipleFileInput(attrs={"multiple": True}))
         super().__init__(*args, **kwargs)
 
+    def clean(self, data, initial=None):
+        if not data:
+            return []
+
+        if isinstance(data, (list, tuple)):
+            return [super(MultipleFileField, self).clean(file, initial) for file in data]
+
+        return [super().clean(data, initial)]
+
 
 class ProductForm(forms.ModelForm):
-    extra_images = MultipleFileField(required=False)
+    extra_images = MultipleFileField(
+        required=False,
+        label="Extra Product Images"
+    )
 
     class Meta:
         model = Product
